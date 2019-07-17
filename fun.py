0e9cbe026ef5c7f778792369409eee8398c4c951
bryte-kop <mailto:kme6019@gmail.com>
2019-07-17 21:28:36 +0000

    Day Three

---
+++ fun.py
@@ -0,0 +1,11 @@
+act1=['ac:001','fank','andor','savings','balance':200]
+act2=['ac:002','adwoa','owusu','current',100]
+
+amount = act1.pop
+
+def deposit(amount,deposit_amount):
+	return amount + deposit_amount
+
+def withdraw(amount,withdraw_amount):
+	return amount - withdraw_amount
+
---
+++ list.py
@@ -0,0 +1,5 @@
+act1=['ac:001','fank','andor','savings',200]
+act2=['ac:002','adwoa','owusu','current',100]
+
+bnk_del = [act1,act2]
+print(bnk_del[0])
