2f9a4e1c7416aea847f132be3a3fd05a24df3792
bryte-kop <mailto:kme6019@gmail.com>
2019-07-23 11:38:34 +0000

    Cart system

---
+++ cart.py
@@ -0,0 +1,12 @@
+li = []
+print("Cart System")
+a = input("enter your first iterm: ")
+b = input("enter your secound iterm: ")
+c = input("enter your third iterm: ")
+
+li.append(a)
+li.append(b)
+li.append(c)
+
+print("Your Cart Iterms are")
+print(li)
